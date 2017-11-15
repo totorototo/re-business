5e1f555bafacccad768fb22a75bf45fd9f5856dd
Toto RoToto <el.totorototo@gmail.com>
2017-11-15 19:55:55 +0100

    minor fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,11 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        index: './lib/es6/src/index.js'
+        index: './lib/es6/src/Index.js'
     },
     output: {
         path: path.join(__dirname, 'dist'),
         filename: '[name].js',
-        library: "re-business",
-        libraryTarget: "umd"
+        library: "re-business"
     }
 };
